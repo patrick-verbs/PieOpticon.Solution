10c6829b171799752c07618793e409abf35d7eb8
Patrick Lee <patricklee1138@gmail.com>
2021-05-14 13:13:40 -0700

    :white_check_mark: Order constructor creates instance of Order

---
+++ PieOpticon.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,115 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using PieOpticon.Models;
+
+namespace PieOpticon.Test
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderCtor_CreatesInstanceOf_Order()
+    {
+      // Arrange
+      Vendor testVendor = new Vendor(
+        "Second-Rate Baked Goods",
+        "Second-stalest second-hand baked goods in the state's second-most-populated city!"
+      );
+      string orderTitle = "Bread x 48; Pastry x 24";
+      string date = "2021-05-14";
+      int vendorId = testVendor.Id;
+      int price = 180;// == (48 * 3) + (24 * 1.5)
+      // Act
+      Order testOrder = new Order(orderTitle, date, vendorId, price);
+      // Assert
+      Assert.AreEqual(typeof(Order), testOrder.GetType());
+    }
+
+    // [TestMethod]
+    // public void GetName_ReturnsName_String()
+    // {
+    //   // Arrange
+    //   string inputName = "Second-Rate Baked Goods";
+    //   Order testOrder = new Order(inputName, "untested description");
+    //   // Act
+    //   string outputName = testOrder.Name;
+    //   // Assert
+    //   Assert.AreEqual(inputName, outputName);
+    // }
+
+    // [TestMethod]
+    // public void GetDescription_ReturnsDescription_String()
+    // {
+    //   // Arrange
+    //   string inputDescription = "Second-stalest second-hand baked goods in the state's second-most-populated city!";
+    //   Order testOrder = new Order("untested name", inputDescription);
+    //   // Act
+    //   string outputDescription = testOrder.Description;
+    //   // Assert
+    //   Assert.AreEqual(inputDescription, outputDescription);
+    // }
+
+    // [TestMethod]
+    // public void GetAll_ReturnsEmptyList_OrderList()
+    // {
+    //   // Arrange
+    //   List<Order> testList = new List<Order> {};
+    //   // Act
+    //   List<Order> result = Order.GetAll();
+    //   // Assert
+    //   CollectionAssert.AreEqual(testList, result);
+    // }
+
+    // [TestMethod]
+    // public void GetAll_ReturnsOrders_OrderList()
+    // {
+    //   // Arrange
+    //   string inputName = "Second-Rate Baked Goods";
+    //   string inputDescription = "Second-stalest second-hand baked goods in the state's second-most-populated city!";
+    //   Order testOrder = new Order(inputName, inputDescription);
+    //   List<Order> expectedList = new List<Order>
+    //   {
+    //     testOrder
+    //   };
+    //   // Act
+    //   List<Order> outputList = Order.GetAll();
+    //   // Assert
+    //   CollectionAssert.AreEqual(expectedList, outputList);
+    // }
+
+    // [TestMethod]
+    // public void GetId_OrdersInstantiateWithAnIdAndGetterReturns_Int()
+    // {
+    // // Arrange
+    // string inputName = "Twice-Baked Goods";
+    // string inputDescription = "Our supplier bakes them! We bake them again! You get them TWICE as fresh!";
+    // Order testOrder = new Order(inputName, inputDescription);
+    // // Act
+    // int outputId = testOrder.Id;
+    // // Assert
+    // Assert.AreEqual(1, outputId);
+    // }
+
+    // [TestMethod]
+    // public void Find_ReturnsCorrectOrder_Order()
+    // {
+    // // Arrange
+    // string inputName1 = "Second-Rate Baked Goods";
+    // string inputDescription1 = "Second-stalest second-hand baked goods in the state's second-most-populated city!";
+    // string inputName2 = "Twice-Baked Goods";
+    // string inputDescription2 = "Our supplier bakes them! We bake them again! You get them TWICE as fresh!";
+    // Order testOrder1 = new Order(inputName1, inputDescription1);
+    // Order testOrder2 = new Order(inputName2, inputDescription2);
+    // // Act
+    // Order foundOrder = Order.Find(2);
+    // // Assert
+    // Assert.AreEqual(testOrder2, foundOrder);
+    // }
+  }
+}
---
+++ PieOpticon/Models/Order.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+
+namespace PieOpticon.Models
+{
+  public class Order
+  {
+    private static List<Order> _allOrders = new List<Order> {};
+    public int Id { get; }
+    public int VendorId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    public List<Vendor> Vendors { get; set; }
+
+    public Order (string orderTitle, string date, int vendorId, int price)
+    {
+      Title = orderTitle;
+      Date = date;
+      VendorId = vendorId;
+      Price = price;
+      Description = $"{Date} | {Vendor.Find(vendorId).Name} | ${Price}";
+      _allOrders.Add(this);
+      Id = _allOrders.Count;
+      Vendors = new List<Vendor>{};
+    }
+
+    // public static List<Order> GetAll()
+    // {
+    //   return _allOrders;
+    // }
+
+    // public static Order Find(int searchId)
+    // {
+    //   return _allOrders[searchId - 1];
+    // }
+
+    public static void ClearAll()
+    {
+      _allOrders.Clear();
+    }
+  }
+}
